d029d4ccc5bd02bb085a05b1683c53af56c21ddf
Anatoly Policastro <32590903+anatoly12@users.noreply.github.com>
2026-02-12 06:11:55 -0500

    Integrate ReactGA for analytics tracking

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,14 @@ import 'react-hot-loader/patch'
 import { AppContainer } from 'react-hot-loader'
 import store from './store'
 import RedBox from 'redbox-react'
+import ReactGA from 'react-ga';
 
 /* * Components * */
 import App from './components/App'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin()
+ReactGA.initialize('UA-XXXXXXXXX-X'); // Replace with real tracking ID
 
 const consoleErrorReporter = ({error}) => {
   console.error(error)
@@ -25,6 +27,7 @@ consoleErrorReporter.propTypes = {
 
 /* * wrapping App.js in Proivder component to allow access to our redux store * */
 const render = function (Component) {
+  ReactGA.pageview(window.location.pathname + window.location.search)
   ReactDOM.render(
     <AppContainer errorReporter={consoleErrorReporter}>
       <Component />
